d06d9729d46bb5d334043f131d43eb1ddadc21d2
ZsZse <kankalinvirag@gmail.com>
2016-04-06 01:54:35 +0200

    Create PreziReader.java

---
+++ src/main/java/hu/mta/sztaki/lpds/cloud/simulator/helpers/trace/file/PreziReader.java
@@ -0,0 +1,36 @@
+package hu.mta.sztaki.lpds.cloud.simulator.helpers.trace.file;
+
+import java.lang.reflect.InvocationTargetException;
+
+import hu.mta.sztaki.lpds.cloud.simulator.helpers.job.Job;
+
+public class PreziReader extends TraceFileReaderFoundation{
+
+	public PreziReader(String fileName, int from, int to,
+					           boolean allowReadingFurther, Class<? extends Job> jobType)
+					           throws SecurityException, NoSuchMethodException{
+		super("Prezi", fileName, from, to, allowReadingFurther, jobType);
+		
+	}
+	
+	@Override
+	public boolean isTraceLine(String line) {
+		return basicTraceLineDetector("#", line);
+	}
+	
+	@Override
+	protected Job createJobFromLine(String line)
+		throws IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException {
+		final String[] fragments = line.trim().split("\\s+");
+		
+		long submittime = Long.parseLong(fragments[0]);
+		long runtime = Long.parseLong(fragments[4]);
+		
+		return jobCreator.newInstance(fragments[1], //id
+									                submittime,   //submit time
+									                null,         //wait time ?
+									                runtime,      //run time
+									                1, -1, -1,    
+									                "USER","GROUP","EXEC", null, 0);	
+	}
+}
