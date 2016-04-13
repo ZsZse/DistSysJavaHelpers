39b2a034300feaac91c68b5dfe6632c397a3163e
ZsZse <kankalinvirag@gmail.com>
2016-04-13 02:53:17 +0200

    Create ReaderContext.java

---
+++ src/main/java/hu/mta/sztaki/lpds/cloud/simulator/helpers/trace/file/ReaderContext.java
@@ -0,0 +1,23 @@
+package hu.mta.sztaki.lpds.cloud.simulator.helpers.trace.file;
+
+import java.lang.reflect.InvocationTargetException;
+
+import hu.mta.sztaki.lpds.cloud.simulator.helpers.job.Job;
+
+public class ReaderContext {
+	
+	private TraceFileReaderFoundation strategy;
+	//private ReaderStrategy strategy;
+	
+	public ReaderContext(TraceFileReaderFoundation strategy){
+		this.strategy=strategy;
+	}
+    
+	//public void setReaderStrategy(..){..}
+	
+	public Job executeStrategy(String line)
+			throws IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException
+	{
+		return strategy.createJobFromLine(line);
+	}
+}
