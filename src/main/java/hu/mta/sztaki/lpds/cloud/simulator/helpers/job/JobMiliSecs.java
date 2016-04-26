ec9bbe21babfc5157c6d84fe84be8feeab1fc201
ZsZse <kankalinvirag@gmail.com>
2016-04-27 01:49:55 +0200

    Update JobMiliSecs.java

--- src/main/java/hu/mta/sztaki/lpds/cloud/simulator/helpers/job/JobMiliSecs.java
+++ src/main/java/hu/mta/sztaki/lpds/cloud/simulator/helpers/job/JobMiliSecs.java
@@ -2,13 +2,7 @@ package hu.mta.sztaki.lpds.cloud.simulator.helpers.job;
 
 public class JobMiliSecs extends Job{
 	
-	public JobMiliSecs(String id, long submit, long queue, long exec, int nprocs,
-		      double ppCpu, long ppMem, String user,
-		      String group, String executable, Job preceding, long delayAfter){
-	
-		super(id,submit,queue,exec,nprocs,ppCpu,ppMem,
-				user,group,executable,preceding,delayAfter);	
-	
+	public JobMiliSecs(){
 		type="MiliSeconds";
 	}
 
