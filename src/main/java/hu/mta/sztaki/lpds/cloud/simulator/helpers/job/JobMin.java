f464d8843812dc9db4e3e5dcb4aa2eaee281aed2
ZsZse <kankalinvirag@gmail.com>
2016-04-27 01:50:44 +0200

    Update JobMin.java

--- src/main/java/hu/mta/sztaki/lpds/cloud/simulator/helpers/job/JobMin.java
+++ src/main/java/hu/mta/sztaki/lpds/cloud/simulator/helpers/job/JobMin.java
@@ -2,13 +2,7 @@ package hu.mta.sztaki.lpds.cloud.simulator.helpers.job;
 
 public class JobMin extends Job{
 	
-	public JobMin(String id, long submit, long queue, long exec, int nprocs,
-			      double ppCpu, long ppMem, String user,
-			      String group, String executable, Job preceding, long delayAfter){
-		
-		super(id,submit,queue,exec,nprocs,ppCpu,ppMem,
-			  user,group,executable,preceding,delayAfter);	
-		
+	public JobMin(){
 		type="Minute";
 	}
 	
